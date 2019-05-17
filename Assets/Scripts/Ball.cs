d47ac03adb5c136bd8bc6a7b637c694f34490624
KKEV1NN <44778147+KKEV1NN@users.noreply.github.com>
2019-05-17 20:01:28 +0200

    Initial Commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI; 
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class Ball : MonoBehaviour
+{
+    public float speed = 5f;
+    Vector3 startPos;
+    Rigidbody rb;
+
+    public Text p;
+    public Text a;
+    int pScore = 0;
+    int aScore = 0; 
+
+    private void Start() {
+       
+        startPos = transform.position;
+        rb = GetComponent<Rigidbody>();
+        Play();
+        
+    }
+    
+    void Play()
+    {
+        float x = Random.Range(0, 2) == 0 ? -1 : 1;
+        float z = Random.Range(0, 2) == 0 ? -1 : 1;
+       
+        rb.velocity = new Vector3(x, 0f, z) * speed;
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name == "G1")
+        {
+            StartCoroutine("Reset");
+            aScore++;
+            a.text = aScore.ToString(); 
+        }
+
+        if (col.gameObject.name == "G2")
+        {
+            StartCoroutine("Reset");
+            pScore++;
+            p.text = pScore.ToString();
+        }
+    }
+
+    IEnumerator Reset()
+    {
+        transform.position = startPos;
+        Debug.Log("1");
+        rb.velocity = new Vector3(0, 0f, 0);
+        yield return new WaitForSeconds(3f);
+        Play();
+    }
+
+
+}
---
+++ Assets/Scripts/Bumper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bumper : MonoBehaviour
+{
+    float Speed = 5;
+    public bool IsPlayer;
+    float zPos = Mathf.Clamp(0, -8, 8);
+    Vector3 dir;
+    public GameObject ball;
+
+    private void Update()
+    {
+        if (IsPlayer)
+        {
+            transform.Translate(0f, 0f, Input.GetAxis("Vertical") * Speed * Time.deltaTime);
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Application.Quit(); 
+            }
+        }
+        else if (!IsPlayer)
+        {
+            dir = (ball.transform.position - transform.position).normalized;
+            transform.position += new Vector3(0, 0, dir.z) * Speed * Time.deltaTime;
+        }
+
+    }
+}
